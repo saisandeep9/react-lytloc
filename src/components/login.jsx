4360f9f6925478aa37f34fcd2687cc620c5ad1cf
sai sandeep <mailtosaisandeep@gmail.com>
2020-11-27 09:37:24 +0530

    first commit

---
+++ src/components/login.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+
+// import auth from "../../services/authService";
+
+
+// import { toast } from "react-toastify";
+// import auth from "../services/authService";
+
+import TextField from "@material-ui/core/TextField";
+// import Button from "@material-ui/core/Button";
+import Validation from "./validation";
+import Joi from "joi-browser";
+
+class Login extends Validation {
+  state = {
+    data: {
+      emailId: "",
+      password: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    emailId: Joi.string().required(),
+    password: Joi.string().required().min(5),
+  };
+
+  doSubmit = async () => {
+    console.log(this.state.data);
+    const { data } = this.state;
+//     const success = await auth.login(data);
+//     console.log(success);
+//     if (success) {
+//       toast.success("Successfully login");
+//       window.location = "/";
+//     }
+  };
+
+  render() {
+    const { data, errors } = this.state;
+
+    return (
+      <div>
+            <div className="row m-5">
+           
+          <div className="col-md-8 ">
+     
+          </div>
+
+          <div
+            className="   float-right "
+            // style={{ width: "0%" }}
+          >
+            <form
+              onSubmit={this.handleSubmit}
+              className="m-1 p-1 col-11 col-12 col-md-9 mt-2 "
+            >
+              {/*m-2 col-md-4 col-12*/}
+              <h1 className="text-center "> Login</h1>
+
+              <TextField
+                name="emailId"
+                label="E-mail"
+                color="secondary"
+                size="small"
+                className="m-3 col-xl-10 col-10"
+                onChange={this.handleChange}
+                helperText={errors.lastName}
+                error={errors.lastName}
+                required
+              />
+
+              <TextField
+                name="password"
+                label="Password"
+                color="secondary"
+                size="small"
+                type="password"
+                className="m-3 col-xl-10 col-10"
+                onChange={this.handleChange}
+                helperText={errors.password}
+                error={errors.password}
+                required
+              />
+
+              <center>
+                <button
+                  type="submit"
+                  className=" btn btn-primary  btn-block mb-3 mt-2"
+                >
+                  Sign up
+                </button>
+              </center>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components/token.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+class Token extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+            <div>
+                
+                <h1>
+                    Token
+                </h1>
+</div>
+
+
+         );
+    }
+}
+ 
+export default Token;
